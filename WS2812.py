98e2b3728dd710d0abfd79ce5ab1881fa19b03b4
Jorne Roefs <jorneroefs@gmail.com>
2024-10-29 16:24:42 +0100

    Change NeoPixel to PixelStrip

--- WS2812.py
+++ WS2812.py
@@ -1,4 +1,4 @@
-from rpi_ws281x import Adafruit_NeoPixel, Color
+from rpi_ws281x import PixelStrip, Color
 import random
 import time
 
@@ -10,7 +10,7 @@ class WS2812:
         LED_DMA        = 10       # DMA channel to use for generating signal (try 5)
         LED_BRIGHTNESS = 100    # Set to 0 for darkest and 255 for brightest
         LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        self.strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
         self.strip.begin()
         
     def SetRGB(self, color):        
